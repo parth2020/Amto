a013bfe579dfd101be9c2c9f452c2481c97d721d
ParthBhindi <45254591+parth2020@users.noreply.github.com>
2018-11-29 12:48:09 +0530

    Rectangle

---
+++ Program.java
@@ -0,0 +1,45 @@
+public class Rectangle
+{
+	int height;
+	int width;
+}
+
+
+public class Program{
+	
+	public static void main(String [] args){
+	
+	Rectangle r1;
+	r1 = new Rectangle();
+	r1.height = 10;
+	r1.width = 20;
+	System.out.println("Details of Rectangle : \nheight: "+r1.height+"\nwidth:"+r2.width);
+}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
