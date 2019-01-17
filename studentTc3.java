4613e4de3e5bae4d8459227062963c0f6b064495
ParthBhindi <45254591+parth2020@users.noreply.github.com>
2019-01-17 12:47:32 +0530

    Student Details With Static Method

--- studentTc3.java
+++ studentTc3.java
@@ -2,9 +2,10 @@ public class studentTc3
 {
     	int sid;
     	String sname;
-    	static String scoll="Marwadi University";
-   
-   
+    	static String scoll="MU";
+  
+   	static{System.out.println("Static Block Is Invoked Before Main");}
+
     	public studentTc3(int sid,String sname)
 	{
         	this.sid=sid;
@@ -14,19 +15,24 @@ public class studentTc3
    	void display()
 	{
 		System.out.println(" ");
-       		System.out.println("Student Roll Numeber:"+sid);
+       		System.out.println("Student Roll Number: "+sid);
 		System.out.println(" ");
        		System.out.println("Student Name: "+sname);
 		System.out.println(" ");
        		System.out.println("Student Collage: "+scoll);
 		System.out.println(" ");
   	}
-	
-    	public static void main(String[] args) 
+
+   	static void meth()
+   	{
+       		System.out.println("Static Method Can Be Called" + "Without Creating Object");
+   	}
+
+    	public static void main(String[] args)
 	{
-        	studentTc3 s1 = new studentTc3(1,"Parth");
-        	studentTc3 s2 = new studentTc3(2,"Joshi");
-        	studentTc3 s3 = new studentTc3(3,"Neel");
+        	studentTc3 s1 = new studentTc3(1," Parth ");
+        	studentTc3 s2 = new studentTc3(2," Joshi ");
+        	studentTc3 s3 = new studentTc3(3," Neel ");
         	s1.display();
         	s2.display();
         	s3.display();
