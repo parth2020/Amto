cb0f408ef8f8dd46c11c95cf42ed8686bf641c42
ParthBhindi <45254591+parth2020@users.noreply.github.com>
2019-01-17 12:38:13 +0530

    Printing Student Details

---
+++ studentTc3.java
@@ -0,0 +1,34 @@
+public class studentTc3 
+{
+    	int sid;
+    	String sname;
+    	static String scoll="Marwadi University";
+   
+   
+    	public studentTc3(int sid,String sname)
+	{
+        	this.sid=sid;
+        	this.sname=sname;
+    	}
+
+   	void display()
+	{
+		System.out.println(" ");
+       		System.out.println("Student Roll Numeber:"+sid);
+		System.out.println(" ");
+       		System.out.println("Student Name: "+sname);
+		System.out.println(" ");
+       		System.out.println("Student Collage: "+scoll);
+		System.out.println(" ");
+  	}
+	
+    	public static void main(String[] args) 
+	{
+        	studentTc3 s1 = new studentTc3(1,"Parth");
+        	studentTc3 s2 = new studentTc3(2,"Joshi");
+        	studentTc3 s3 = new studentTc3(3,"Neel");
+        	s1.display();
+        	s2.display();
+        	s3.display();
+    	}
+}
